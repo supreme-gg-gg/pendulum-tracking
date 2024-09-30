2b81d7d4aa84b70424603b8cb20a6132c7f1a8c8
UDAYA-0504 <159785668+UDAYA-0504@users.noreply.github.com>
2024-09-30 08:52:59 +0530

    Modify plotting.py to process csv directory (#9)
    * Modified plotting.py added the function to get a directory contating csv and to process them according to your functions
    
    * Bug fix for plotting and new docs
    
    ---------
    
    Co-authored-by: Jet Chiang <jetjiang.ez@gmail.com>

--- plotting.py
+++ plotting.py
@@ -4,7 +4,7 @@ import matplotlib.pyplot as plt
 from scipy.signal import find_peaks
 from scipy.optimize import curve_fit
 import argparse
-import math
+import math, os, glob
 
 
 def binning(trials, max_time, bin_size=1):
@@ -262,7 +262,7 @@ def fit_amplitude(df, ret=False, err=None, output_path="amplitude_decay.png"):
             verticalalignment='top', horizontalalignment='right', bbox=props)
 
     plt.tight_layout(pad=2.0)
-    plt.savefig(f"{output_path}", format="png")
+    plt.savefig(f"output/{output_path}", format="png")
     plt.show()
 
     if ret:
@@ -333,14 +333,24 @@ def amplitude_decay(times, peaks, error, q_factor, output_path="decay_fit.png"):
 
 if __name__ == "__main__":
     
-    parser = argparse.ArgumentParser(description="Plot pendulum angle from CSV data.")
-    parser.add_argument("--path", type=str, default="positions.csv", help="Path to the CSV file containing the data.")
+    parser = argparse.ArgumentParser(description="Plot pendulum angle from CSV data.") 
+    parser.add_argument("--csv_files_path", nargs="*",help="list of csv files to process.")
+    parser.add_argument("--csv_files_directory", type=str, help=" directory containing al csv files containing data.")
     args = parser.parse_args()
-
-    # If called from main you must specify a CSV file path
-    df = pd.read_csv(args.path, header=0)
-    
-    # USAGE: COMMENT OR UNCOMMENT THE FOLLOWING LINES BEFORE RUNNING THIS FILE
-
-    fit_amplitude(df)
-    # plot_angle(df)
+    csv_files=[]
+    if args.csv_files_path:
+        csv_files.extend(args.csv_files_path)
+    if args.csv_files_directory:
+        csv_files.extend(glob.glob(os.path.join(args.csv_files_directory,"*.csv")))
+    if not csv_files:
+        print("no csv files provided, please provide them")
+    else:
+        for i in csv_files:
+            try:
+                df=pd.read_csv(i,header=0)
+                print(f"processing csv file:{i}")
+                #functions to run on each csv file
+                fit_amplitude(df,output_path=f"{os.path.splitext(os.path.basename(i))[0]}_amplitude_decay.png")
+                plot_angle(df,output_path=f"{os.path.splitext(os.path.basename(i))[0]}_angle_graph.png")
+            except Exception as e:
+                print(f"failed processing{i}:{e}")
